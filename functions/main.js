4a4776d3707440e77c5998f97cacea46fa9b48f0
Juan Manuel Malaver <juanmalaver1511@gmail.com>
2025-09-15 20:01:34 -0500

    Actualizar estilos del carrito y mejorar botones de acción

--- functions/main.js
+++ functions/main.js
@@ -54,7 +54,6 @@ function updateCartDisplay() {
     cartBody.innerHTML = "<p id='cartEmpty'>El carrito está vacío</p>";
   } else {
     cartFooter.style.display = 'block';
-    console.log(JSON.stringify(cart));
     cartBody.innerHTML = cart.map(item => `
       <div class="slider-container" style="margin-bottom: 10px; display: flex; align-items: center; justify-content: space-between;">
         <div class="cart-item">
@@ -68,7 +67,7 @@ function updateCartDisplay() {
               <button class="btn-add" onclick="increaseQuantity(${item.id})">+</button>
             </div>
           </div>
-          <button class="btn-trash" onclick="removeFromCart(${item.id})">🗑️</button>
+          <button class="btn-trash" onclick="removeFromCart(${item.id})"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash2-icon lucide-trash-2"><path d="M10 11v6"/><path d="M14 11v6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg></button>
         </div>
       </div>
     `).join('');
